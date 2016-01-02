aae0c601c2f4cdff9a5f07108f25b39a20dc3419
Andres <andres@Andress-Mac.local>
2016-01-02 09:08:47 -0800

    first commit

--- DistanceCalc/ViewController.m
+++ DistanceCalc/ViewController.m
@@ -7,21 +7,55 @@
 //
 
 #import "ViewController.h"
+#import "DistanceGetter/DGDistanceRequest.h"
 
 @interface ViewController ()
+@property (nonatomic) DGDistanceRequest *req;
+
+@property (weak, nonatomic) IBOutlet UITextField *location0;
+@property (weak, nonatomic) IBOutlet UITextField *location1;
+@property (weak, nonatomic) IBOutlet UILabel *distance1;
+@property (weak, nonatomic) IBOutlet UITextField *location2;
+@property (weak, nonatomic) IBOutlet UILabel *distance2;
+@property (weak, nonatomic) IBOutlet UITextField *location3;
+@property (weak, nonatomic) IBOutlet UILabel *distance3;
+@property (weak, nonatomic) IBOutlet UITextField *location4;
+@property (weak, nonatomic) IBOutlet UILabel *distance4;
+@property (weak, nonatomic) IBOutlet UIButton *calculateBtn;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)calculateTapped:(id)sender {
+    self.calculateBtn.enabled = NO;
+    
+    self.req = [DGDistanceRequest alloc];
+    NSString *initialLocation = self.location0.text;
+    NSArray *destination = @[self.location1.text, self.location2.text, self.location3.text, self.location4.text];
+    NSArray *distances = @[self.distance1.text, self.distance2.text, self.distance3.text, self.distance4.text];
+    [self.req initWithLocationDescriptions:destination sourceDescription:initialLocation];
+    
+    __weak ViewController *weakSelf = self;
+    
+    self.req.callback = ^void(NSArray *responses){
+        ViewController *strongSelf = weakSelf;
+        if(!strongSelf) return;
+        
+        for (int i = 0; i == responses.count; i++){
+            if(responses[i]!=nil){
+                strongSelf.distances[i]=[NSString stringWithFormat:@"%.2f km", [responses[i] floatValue]/1000.0]
+            }
+            else{
+                strongSelf.distances[i] = @"Error";
+            }
+        }
+    };
+    
+    self.calculateBtn.enabled = YES;
+    
+    [self.req start];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
